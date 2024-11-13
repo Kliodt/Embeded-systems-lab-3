44c66a2297d619b432b833cdb07d3edaac96a13e
hamzaagaev <hamzaagaev@gmail.com>
2024-11-14 01:59:32 +0300

    Add oled and font files, implement drawSquare and drawLabel

--- src/controllerImplementation.c
+++ src/controllerImplementation.c
@@ -2,6 +2,8 @@
 
 #include "controller.h"
 #include "handlers.h"
+#include "fonts.h"
+#include "oled.h"
 
 struct HandlerInfo allHandlers[BUTTONS_TOTAL] = {0};
 
@@ -34,22 +36,57 @@ void printScreen(void) {
 
 bool initController(void) { return true; };
 
+// пишет текст в прямоугольник, у которого левый верхний угол находится по координатам x, y
+// переносит большой текст на новую строку
+// ОБНОВЛЯЕТ ЭКРАН!!!
 void drawLabel(int x, int y, char str[]) {
     // todo: check screen boundaries
-    // ! this is debug code
-    printf("debug: text at (%d, %d): %s\n", x, y, str);
+    oled_Reset();
+    FontDef defaultFont = Font_7x10;
+    u_int8_t maxSymbolsWidth = (OLED_WIDTH - x) / defaultFont.FontWidth;
+    u_int8_t maxSymbolsHeight = (OLED_HEIGHT - y) / defaultFont.FontHeight;
+    u_int8_t strLen = 0;
+    while(str[strLen] != 0) {
+        strLen++;
+    }
+    u_int16_t maxSymbolsCount = maxSymbolsWidth * maxSymbolsHeight;
+    if (maxSymbolsCount > 0 && strLen <= maxSymbolsCount) {
+        oled_SetCursor(x, y);
+        u_int8_t fullSubstringCount = strLen / maxSymbolsWidth;
+        u_int8_t lastSubstringLen = strLen % maxSymbolsWidth;
+        for (int i = 0; i < fullSubstringCount; i++) {
+            oled_SetCursor(x, y + i * 2 * maxSymbolsHeight);
+            char substring[maxSymbolsWidth + 1];
+            for (int j = 0; j < maxSymbolsWidth; j++) {
+                substring[j] = str[j + i * maxSymbolsWidth];
+            }
+            substring[maxSymbolsWidth] = '\0';
+            oled_WriteString(substring, defaultFont, White);
+        }
+        oled_SetCursor(x, y + fullSubstringCount * maxSymbolsHeight * 2);
+        char lastSubstring[lastSubstringLen + 1];
+        for (int j = 0; j < lastSubstringLen; j++) {
+            lastSubstring[j] = str[j + fullSubstringCount * maxSymbolsWidth];
+        }
+        lastSubstring[lastSubstringLen] = '\0';
+        oled_WriteString(lastSubstring, defaultFont, White);
+        oled_UpdateScreen();
+    }
 }
 
+// рисует квадратик в координатах x, y (левый верхний угол) со стороной sizePx
+// НЕ ОБНОВЛЯЕТ ЭКРАН!!!
 void drawSquare(int x, int y, int sizePx) {
     // todo: check screen boundaries
     // ! this is debug code
     for (int i = y; i < y + sizePx; i++) {
         for (int j = x; j < x + sizePx; j++) {
-            screenMatrix[i][j] = true;
+            oled_DrawPixel(i, j, White);
         }
     }
-};
+}
 
+// не реализовано
 void drawMatrix(bool** matrix, int x, int y, int widthPx, int heightPx) {
     // ! WARNING: didn't test it, but should work
     // todo: check screen boundaries
@@ -61,13 +98,9 @@ void drawMatrix(bool** matrix, int x, int y, int widthPx, int heightPx) {
     }
 }
 
+// очистка экрана
 void clearScreen(void) {
-    // ! this is debug code
-    for (int i = 0; i < SCREEN_HEIGHT_PX; i++) {
-        for (int j = 0; j < SCREEN_WIDTH_PX; j++) {
-            screenMatrix[i][j] = false;
-        }
-    }
+    oled_Reset();
 };
 
 void updateScreen(void) {
